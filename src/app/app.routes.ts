15c7789420131d195cacfc58357b89722aac4b85
Luis Calzadilla <luis.calzadilla198@comunidadunir.net>
2026-02-19 04:43:41 +0100

    creacion de rutas de navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component/home.component';
+import { NewComponent } from './pages/new.component/new.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'new', component: NewComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
